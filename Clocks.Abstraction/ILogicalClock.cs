84d32c9fddce7c48590b6c0d3b0e4d04d2cb8012
Shuai Zhang <zhangshuai.ustc@gmail.com>
2017-12-31 21:08:04 +0800

    Add documents for Clocks.Abstraction

--- Clocks.Abstraction/ILogicalClock.cs
+++ Clocks.Abstraction/ILogicalClock.cs
@@ -2,9 +2,20 @@ using System;
 
 namespace Clocks
 {
+    /// <summary>
+    /// Basic interface for a logical clock
+    /// <para>Typical logical clocks are <strong>lamport scalar clock</strong> &amp; <strong>vector clock</strong></para>
+    /// </summary>
+    /// <typeparam name="T">The concrate type of time point</typeparam>
     public interface ILogicalClock<T>
         where T: IComparable, IComparable<T>, IEquatable<T>
     {
+        /// <summary>
+        /// Get current time point.
+        /// </summary>
+        /// <value>
+        /// The current time point.
+        /// </value>
         T Now { get; }
     }
 }
