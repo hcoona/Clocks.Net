dd8d95d542d016dafbef5d17f78c6888ebce3a3d
Shuai Zhang <zhangshuai.ustc@gmail.com>
2017-12-31 21:19:48 +0800

    Add documents for Clocks.LamportClock

--- Clocks.LamportClock/LamportClock.cs
+++ Clocks.LamportClock/LamportClock.cs
@@ -2,12 +2,21 @@ using System.Threading;
 
 namespace Clocks
 {
+    /// <summary>
+    /// The Lamport scalar logical clock. Check the paper "Time, Clocks, and the Ordering of Events in a Distributed System" for further details.
+    /// <para>The type is thread-safe.</para>
+    /// </summary>
+    /// <seealso cref="Clocks.ILogicalClock{System.Int64}" />
     public class LamportClock : ILogicalClock<long>
     {
         private long counter;
 
         public LamportClock() : this(0) { }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LamportClock"/> class with given value as its internal counter initial value.
+        /// </summary>
+        /// <param name="initial">The internal counter initial value.</param>
         public LamportClock(long initial)
         {
             counter = initial;
@@ -15,11 +24,24 @@ namespace Clocks
 
         public long Now => Interlocked.Read(ref counter);
 
+        /// <summary>
+        /// Increments the internal counter.
+        /// <para><strong>IR1.</strong>Each process P<sub>i</sub> increments C<sub>i</sub> between any two successive events.</para>
+        /// </summary>
+        /// <returns>The origin counter value.</returns>
         public long Increment()
         {
             return Interlocked.Increment(ref counter);
         }
 
+        /// <summary>
+        /// Witnesses another timepoint.
+        /// <para><strong>IR2.</strong>(a) If event a is the sending of a message m by process P<sub>i</sub>,
+        /// then the message m contains a timestamp T<sub>m</sub> = C<sub>i</sub>< a >.
+        /// (b) Upon receiving a message m, process P<sub>j</sub> sets C<sub>j</sub> greater than or equal to its present value and greater than T<sub>m</sub>.</para>
+        /// </summary>
+        /// <param name="timepoint">The timepoint.</param>
+        /// <returns>The new counter value.</returns>
         public long Witness(long timepoint)
         {
             while (true)
